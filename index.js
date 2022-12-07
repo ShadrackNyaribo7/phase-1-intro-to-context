6d29098d2dcbb75fd2153dd8a9687150372c4f33
shadracknyaribo <stilespulsar77@gmail.com>
2022-12-07 09:30:33 -0800

    function of employee data created

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function createEmployeeRecord (firstname,familyname,title,payrateperhour){
+employee = {};
+employee.firstname = firstname;
+employee.familyname = familyname;
+employee.title = title;
+employee.payrateperhour = payrateperhour;
+return employee;
+
+}
