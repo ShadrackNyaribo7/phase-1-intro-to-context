c77b8f1705a890c68cdb700f6777d2ba2fc01d19
shadracknyaribo <stilespulsar77@gmail.com>
2022-12-07 10:17:38 -0800

    first attempt

--- index.js
+++ index.js
@@ -8,3 +8,44 @@ employee.payrateperhour = payrateperhour;
 return employee;
 
 }
+ function createEmployeeRecords (employee,createEmployeeRecord) {
+newemployee[employee] = employee.createEmployeeRecord;
+return newemployee;
+ }
+
+function createTimeInEvent(employee,datestamp){
+    employeeRecord:
+    type:  TimeIn;
+    hour: datestamp
+    date: datestamp
+    return employeeRecord;
+}
+   function createTimeOutEvent (employee,datestamp){
+        employeeRecord:
+        type:  TimeOut;
+        hour: datestamp
+        date: datestamp
+        return employeeRecord;
+}
+function hoursWorkedOnDate (employee,datestamp){
+    hoursWorked:
+    type: Int;
+    let hoursLapsed = TimeOut - TimeIn;
+    return hoursLapsed;
+
+}
+function wagesEarnedOnDate (employee,datestamp){
+    let payOwed = hoursWorkedOnDate * recordsPayRate;
+    return payOwed;
+
+}
+
+function allWagesFor (employee,payOwed){
+    let totalpayOwed = wagesEarnedOnDate * payOwed;
+return totalpayOwed;
+}
+
+function calculatePayroll (newemployee){
+   let totalWages =  wagesEarnedOnDate * totalpayOwed;
+   return totalWages;
+}
